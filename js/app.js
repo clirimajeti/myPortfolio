dd18ea4a9f7691a72a59961452e1f44a98f22de9
unknown <clirimajeti@gmail.com>
2019-02-11 18:44:50 +0100

    add toggle effect

---
+++ js/app.js
@@ -0,0 +1,16 @@
+// FullPageJs Script
+    new fullpage('#fullpage', {
+            autoScrolling:true,
+            responsiveWidth:480,
+            anchors: ['section1', 'section2', 'section3', 'section4'],
+            menu: '#menu',
+            paddingTop: '5em',
+            });
+        fullpage_api.setAllowScrolling(true);
+
+// Toggle Script
+    const ball = document.querySelector('.ball');
+        ball.addEventListener('click', () => {
+            ball.classList.toggle('night');
+            document.body.classList.toggle('black');
+        })
