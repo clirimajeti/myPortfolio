d620a1916faa83e5dec57e5b0c38c2c4440d307a
unknown <clirimajeti@gmail.com>
2019-02-20 23:28:36 +0100

    SEO

--- js/app.js
+++ js/app.js
@@ -1,13 +1,4 @@
-// FullPageJs Script
-    new fullpage('#fullpage', {
-            autoScrolling:true,
-            responsiveWidth:480,
-            anchors: ['section1', 'section2', 'section3', 'section4'],
-            menu: '#menu',
-            paddingTop: '5em',
-            });
-        fullpage_api.setAllowScrolling(true);
-
+ScrollOut();
 // Toggle Script
     const ball = document.querySelector('.ball');
         ball.addEventListener('click', () => {
@@ -15,7 +6,7 @@
             document.body.classList.toggle('black');
         })
 
-        document.getElementsByTagName('video')[0].onended = function () {
-            this.load();
-            this.play();
-          };
+    document.getElementsByTagName('video')[0].onended = function () {
+        this.load();
+        this.play();
+        };
